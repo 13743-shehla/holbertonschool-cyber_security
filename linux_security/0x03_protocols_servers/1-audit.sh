391179754a165f96bee167dd9b19b0cd586576ea
shehla <13743@holbertonstudents.com>
2026-05-06 08:38:24 +0000

    task 1

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# This script filters the sshd_config file to report only active, non-standard settings
+# by removing comments and empty lines.
 
 grep -v '^#' /etc/ssh/sshd_config | grep -v '^$'
