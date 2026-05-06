b07750ab659e33b4f44b02ab0c5d50f109c80cf9
shehla <13743@holbertonstudents.com>
2026-05-06 08:53:33 +0000

    task 1

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# Audit SSH configuration by displaying only active settings.
-# Exclude lines starting with # (even with spaces) and empty lines.
 grep -E -v '^\s*#|^\s*$' /etc/ssh/sshd_config
