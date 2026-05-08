98ba8c154e831c172aaf6188231e6cfc5965ac82
shehla <13743@holbertonstudents.com>
2026-05-08 13:56:04 +0000

    task 2

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-targets=$(find /home /var/www -type d -perm -2 2>/dev/null)
-
-for dir in $targets; do
-    echo "$dir"
-    
-    chmod 755 "$dir"
-done
+find / -type d -perm -0002 -print -exec chmod 755 {} + 2>/dev/null
