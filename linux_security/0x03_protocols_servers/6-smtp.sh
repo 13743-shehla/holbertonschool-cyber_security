275b1f9623200ff356078a88fa82dbd870c9dd77
shehla <13743@holbertonstudents.com>
2026-05-08 14:26:13 +0000

    task 6

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-smtpd_tls_security_level=$(grep "smtpd_tls_security_level" /etc/postfix/main.cf || echo "STARTTLS not configured")
-echo "$smtpd_tls_security_level"
+postconf smtpd_tls_security_level
