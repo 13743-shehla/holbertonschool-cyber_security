0a8be569d03627a7416dea04fd3f83ce74adad45
shehla <13743@holbertonstudents.com>
2026-06-17 05:22:29 -0400

    task0

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,21 +1,2 @@
 #!/bin/bash
-
-# 1. Check if an argument (subnetwork) was provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <subnetwork>"
-    echo "Example: $0 192.168.65.0/24"
-    exit 1
-fi
-
-# 2. Check if the script is running as root/privileged user
-if [ "$EUID" -ne 0 ]; then
-    echo "Error: This script must be run as root or with sudo."
-    exit 1
-fi
-
-SUBNET=$1
-
-# 3. Execute Nmap with specific flags:
-# -PR: Forces ARP ping (host discovery)
-# -sn: Disables port scanning after host discovery
-nmap -PR -sn "$SUBNET"
+sudo nmap -sn -PR $1
