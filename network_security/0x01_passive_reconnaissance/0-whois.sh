539a970d6c1f54d190a80423db0eac3c2e2ea0a6
shehla <13743@holbertonstudents.com>
2026-06-19 05:46:31 -0400

    task0

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F
+whois "$1" | awk -F': ' 'BEGIN{OFS=","} /^Registrant|^Admin|^Tech/{k=$1; gsub(/ /, "", k); if(k ~ /Street$/)$2=$2" "; if(k ~ /Ext$/)k=k":"; print k,$2}' | awk 'ORS=NR==1?"":RS' RS="\n" > "$1.csv"
