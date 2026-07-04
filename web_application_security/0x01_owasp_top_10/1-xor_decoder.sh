12a87ec2f24ef0096753dd63f05a263f8278a6c8
shehla <13743@holbertonstudents.com>
2026-07-04 09:23:16 -0400

    task 0

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo -n "${1#*\{xor\}}" | base64 -d | tr '!-~' '_-~ !-^'
+# Decode WebSphere XOR encoded string accurately
+echo -n "${1#*\{xor\}}" | base64 -d | xxd -p -c1 | while read -r hex; do
+    printf "\\x$(printf "%02x" $((0x$hex ^ 0x5f)))"
+done
