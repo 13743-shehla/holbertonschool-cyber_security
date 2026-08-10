75dfb4ed957cdfcf3287102f5e8ed6bf3cd93177
shehla <13743@holbertonstudents.com>
2026-08-10 05:48:53 -0400

    2-accounts.sh

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-tail -1000 auth.log | grep -i "root" | grep -E "Failed|Accepted"
+tail -1000 auth.log | awk '
+/sshd.*Failed password/ {
+    failed[$9]++
+}
+/sshd.*Accepted password/ {
+    if (failed[$9] >= 1 && !found[$9]) {
+        print $9
+        found[$9] = 1
+    }
+}'
