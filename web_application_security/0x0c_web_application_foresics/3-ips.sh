a723c4469f86a7e3a1f200f1358e8b7e161e0ca8
shehla <13743@holbertonstudents.com>
2026-08-16 08:18:50 -0400

    task 3

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep "Accepted password" auth.log | grep -oP 'from \K[0-9.]+' | sort -u | wc -l
+grep "Accepted password for root" auth.log | grep -oP 'from \K[0-9.]+' | sort -u | wc -l
